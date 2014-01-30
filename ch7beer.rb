d97b6c50afed4ff3664b0d72c64b0ed9f9fc2986
Courtney Hegwer <thegwer@hotmail.com>
2014-01-29 22:57:25 -0600

    beer song

---
+++ ch7beer.rb
@@ -0,0 +1,8 @@
+beer = 99
+while beer > 0
+	puts "#{beer} bottles of beer on the wall,"
+	puts "#{beer} bottles of beer!"
+	puts "Take one down, and pass it around,"
+	beer = beer -= 1
+	puts "#{beer} bottles of beer on the wall!"
+end
