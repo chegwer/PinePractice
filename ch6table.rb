8893d67511eb11a0173b518a70dd43274014c01a
Courtney Hegwer <thegwer@hotmail.com>
2014-01-29 22:57:00 -0600

    table of contents

---
+++ ch6table.rb
@@ -0,0 +1,18 @@
+#table of contents
+line_width = 70
+title = "Table of Contents"
+ch1 = "Chapter 1:   Getting Started"
+pg1 = "page  1"
+ch2 = "Chapter 2:   Numbers"
+pg2 = "page  9"
+ch3 = "Chapter 3:   Letters"
+pg3 = "page  13"
+
+puts(title.center(line_width))
+puts(ch1.ljust(line_width))
+puts(pg1.rjust(line_width))
+puts(ch2.ljust(line_width))
+puts(pg2.rjust(line_width))
+puts(ch3.ljust(line_width))
+puts(pg3.rjust(line_width))
+
