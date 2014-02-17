4eedff3a213548ca2c5b739856af67adcab5e61e
Courtney Hegwer <thegwer@hotmail.com>
2014-02-17 17:36:40 -0600

    seconds in a life

---
+++ ch12sec.rb
@@ -0,0 +1,12 @@
+t1 = Time.local(1991, 2, 27, 5, 55)
+t2 = Time.new
+
+diff = t2 - t1 #difference between now and born
+
+bil = 1000000000
+
+deef = bil - diff #difference between a bil yrs and length of life
+
+#useless
+
+puts(Time.gm(1991, 2, 27, 5, 55) + 10 ** 9)
